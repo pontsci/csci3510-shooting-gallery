738921a2e4b65828e5cc6c3b50fe607f01406f29
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-22 17:46:22 -0500

    begin gun script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float range = 100f;
+
+    private Camera fpsCamera;
+    private float nextTimeToFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsCamera = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+        nextTimeToFire = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool ready = Time.time >= nextTimeToFire;
+        if(ready && Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("hit: " + hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if(target != null)
+            {
+                target.Process(hit);
+            }
+
+            nextTimeToFire = Time.time + 1f;
+        }
+    }
+}
