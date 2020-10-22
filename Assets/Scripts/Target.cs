32d76dc8900a2f7cb869b38f66b3c5a78acf6d49
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-22 16:30:39 -0500

    add target prefab, sci-fi weapons pack omitted to safe github file space

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject target;
+
+    public GameObject effectsManager;
+    public GameObject hitEffect;
+    public float effectDuration = 0.1f;
+
+    public AudioClip hitSound;
+
+    protected Effect effectScript;
+
+    private void Awake()
+    {
+        //init must occur in awake method
+        effectScript = effectsManager.GetComponent<Effect>();
+    }
+
+    public virtual void Process(RaycastHit hit)
+    {
+
+    }
+}
---
+++ Assets/Scripts/TargetCube.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class TargetCube : Target
+{
+    bool rotationOn = false;
+    float rotationAmount = 360f;
+    float currentRotationAmount = 0f;
+
+    Vector3 rotation;
+
+    float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotationOn)
+        {
+            if(currentRotationAmount < rotationAmount)
+            {
+                time = Time.deltaTime;
+                target.transform.Rotate(rotation * time);
+                currentRotationAmount += rotationAmount * time;
+            }
+            else
+            {
+                rotationOn = false;
+                target.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+        }
+    }
+
+    void Rotate(float degrees)
+    {
+        if (!rotationOn)
+        {
+            Debug.Log("init rotation for: " + target.name);
+            rotationOn = true;
+            rotationAmount = degrees;
+            currentRotationAmount = 0f;
+            rotation = new Vector3(0, degrees, 0);
+        }
+    }
+
+    public override void Process (RaycastHit hit)
+    {
+        Debug.Log("In Process, hit: " + gameObject.tag);
+
+        if(gameObject.tag == "Target")
+        {
+            Rotate(rotationAmount);
+        }
+
+        effectScript.Play(hit, hitSound, hitEffect, effectDuration);
+    }
+}
