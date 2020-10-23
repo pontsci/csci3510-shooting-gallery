ba3b926d2b5e737d7d6c1a23156b1a1b4c6a2694
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-22 23:45:08 -0500

    add portal, remove polyperfect gitignore line

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
+    public GameObject fpsController;
+    public GameObject terrain;
+
+    SpawnPoint spawnPoint;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPoint = new SpawnPoint();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.tag == "Player")
+        {
+            Vector3 position = spawnPoint.Generate(other.gameObject, terrain);
+            MoveTo(other.gameObject, position);
+        }
+    }
+
+    void MoveTo(GameObject player, Vector3 position)
+    {
+        CharacterController controllerScript = player.GetComponent<CharacterController>();
+        controllerScript.enabled = false;
+        player.transform.position = position;
+        controllerScript.enabled = true;
     }
 }
