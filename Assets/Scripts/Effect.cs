2ed6ef02b5b7f81a5450d756e95ce2315bc4d1af
Mason Flemingm <mgfleming@unomaha.edu>
2020-10-22 19:22:11 -0500

    create effect script, add audio files

--- Assets/Scripts/Effect.cs
+++ Assets/Scripts/Effect.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class Effect : MonoBehaviour
 {
-   
+    AudioSource effectAudio;
+
+    GameObject particleEffect;
+    float volume;
+
+    private void Start()
+    {
+        effectAudio = gameObject.GetComponent<AudioSource>();
+        volume = 0.7f;
+    }
+
+    public void Play(RaycastHit hit, AudioClip hitSound, GameObject hitEffect, float effectDuration)
+    {
+        if(hitEffect != null)
+        {
+            particleEffect = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(particleEffect, effectDuration);
+        }
+        effectAudio.PlayOneShot(hitSound, volume);
+    }
 }
